9ca41f9bfd132088de85e4bdff57ace317fa3ee9
Cenk Gündoğan <mail+dev@gundogan.net>
2019-02-08 13:38:33 +0100

    test: add initial infrastructure for unit tests

---
+++ test/test.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2019 HAW Hamburg
+ *
+ */
+
+#include "unity.h"
+#include "ndnup.h"
+
+void test_1(void)
+{
+    TEST_FAIL();
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+ 
+    RUN_TEST(test_1);
+ 
+    return UNITY_END();
+}
