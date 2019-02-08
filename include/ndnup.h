ea67eb556966e08402b2d7ca34a17231552665fd
Cenk Gündoğan <mail+dev@gundogan.net>
2019-02-08 14:25:52 +0100

    doc: initial doxygen config

--- include/ndnup.h
+++ include/ndnup.h
@@ -1,1 +1,21 @@
+/*
+ * Copyright (C) 2019 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @file        ndnup.h
+ * @brief       NDNup functions
+ * @author      Cenk Gündoğan <cenk.guendogan@haw-hamburg.de>
+ * @copyright   MIT License
+ * @addtogroup  NDNup
+ * @{
+ */
 void test(void);
+
+/**
+ * @}
+ */
