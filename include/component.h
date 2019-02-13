cbd12c10bf40d9528d38c1c888cbe90abb8d9eaa
Michael Frey <michael.frey@msasafety.com>
2019-02-13 13:14:41 +0100

    ndnup: added name component types

---
+++ include/component.h
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2019 Safety IO 
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @file        component.h
+ * @brief       Data structures and functions for NDN name components
+ * @author      Michael Frey <michael.frey@safetyio.com>
+ * @copyright   MIT License
+ * @addtogroup  NDNup Minimal Standalone NDN Packet Parser
+ * @{
+ */
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+#include <limits.h>
+#include <stdint.h>
+
+#include "constants.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef NDN_INVALID_COMPONENT
+#define NDN_INVALID_COMPONENT                    0u /**< invalid name component type */ 
+#endif 
+
+#ifndef NDN_IMPLICIT_SHA256_DIGEST_COMPONENT
+#define NDN_IMPLICIT_SHA256_DIGEST_COMPONENT     1u /**< implicit SHA-256 digest component */
+#endif 
+
+#ifndef NDN_PARAMETERS_SHA256_DIGEST_COMPONENT
+#define NDN_PARAMETERS_SHA256_DIGEST_COMPONENT	 2u /**< SHA-256 digest of Interest Parameters */	
+#endif 
+
+#ifndef NDN_GENERIC_NAME_COMPONENT
+#define NDN_GENERIC_NAME_COMPONENT               8u /**< generic name component */	
+#endif 
+
+#ifndef NDN_KEYWORD_NAME_COMPONENT
+#define NDN_KEYWORD_NAME_COMPONENT              32u /**< well-known keyword */ 
+#endif 
+
+#ifndef NDN_SEGMENT_NAME_COMPONENT
+#define NDN_SEGMENT_NAME_COMPONENT              33u /**< segment number */	
+#endif 
+
+#ifndef NDN_BYTE_OFFSET_NAME_COMPONENT
+#define NDN_BYTE_OFFSET_NAME_COMPONENT		    34u /**< byte offset	*/
+#endif 
+
+#ifndef NDN_VERSION_NAME_COMPONENT
+#define NDN_VERSION_NAME_COMPONENT              35u /**< version number */ 
+#endif 
+
+#ifndef NDN_TIMESTAMP_NAME_COMPONENT
+#define NDN_TIMESTAMP_NAME_COMPONENT            36u /**< unix timestamp in microseconds */ 
+#endif 
+
+#ifndef NDN_SEQUENCE_NUM_NAME_COMPONENT
+#define NDN_SEQUENCE_NUM_NAME_COMPONENT         37u /**< sequence number */
+#endif 
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* COMPONENT_H */
+
+/**
+ * @}
+ */
