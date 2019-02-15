5f94b0aae6fe1933b6a893c4325954fb4feb7c53
Cenk Gündoğan <mail+dev@gundogan.net>
2019-02-15 16:23:27 +0100

    ndnup: tlv: minor cleanup

--- include/tlv.h
+++ include/tlv.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Safety IO 
+ * Copyright (C) 2019 Safety IO
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -15,8 +15,8 @@
  * @{
  */
 
-#ifndef NDN_UP_TLV_H
-#define NDN_UP_TLV_H
+#ifndef NDNUP_TLV_H
+#define NDNUP_TLV_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -27,38 +27,38 @@ extern "C" {
  *  https://named-data.net/doc/NDN-packet-spec/current/types.html
  */
 typedef enum tlv_type {
-	interest                           = 0x05,
-	data                               = 0x06,
-	name                               = 0x07,
-	generic_Name_Component	           = 0x08,
-	implicit_sha256_digest_component   = 0x01,
-	parameters_sha256_digest_component = 0x02,
-	can_be_prefix	                   = 0x21,
-	must_be_fresh	                   = 0x12,
-	forwarding_hint	                   = 0x1e,
-	nonce	                           = 0x0a,
-	interest_lifetime	               = 0x0c,
-	hop_limit	                       = 0x22,
-	parameters	                       = 0x24,
-	meta_info	                       = 0x14,
-	content	                           = 0x15,
-	signature_info	                   = 0x16,
-	signature_value	                   = 0x17,
-	content_type	                   = 0x18,
-	freshness_period	               = 0x19,
-	final_block_id	                   = 0x1a,
-	signature_type	                   = 0x1b,
-	key_locator                        = 0x1c,
-	key_digest                         = 0x1d,
-	delegation                         = 0x1f,
-	preference                         = 0x1e
+        interest                           = 0x05,
+        data                               = 0x06,
+        name                               = 0x07,
+        generic_Name_Component             = 0x08,
+        implicit_sha256_digest_component   = 0x01,
+        parameters_sha256_digest_component = 0x02,
+        can_be_prefix                      = 0x21,
+        must_be_fresh                      = 0x12,
+        forwarding_hint                    = 0x1e,
+        nonce                              = 0x0a,
+        interest_lifetime                  = 0x0c,
+        hop_limit                          = 0x22,
+        parameters                         = 0x24,
+        meta_info                          = 0x14,
+        content                            = 0x15,
+        signature_info                     = 0x16,
+        signature_value                    = 0x17,
+        content_type                       = 0x18,
+        freshness_period                   = 0x19,
+        final_block_id                     = 0x1a,
+        signature_type                     = 0x1b,
+        key_locator                        = 0x1c,
+        key_digest                         = 0x1d,
+        delegation                         = 0x1f,
+        preference                         = 0x1e
 } tlv_types_t;
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* NDN_UP_TLV_H */
+#endif /* NDNUP_TLV_H */
 
 /**
  * @}
