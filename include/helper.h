3cd2bfa7dab38b9a8cb834df799c75d05cc6dbbc
Michael Frey <michael.frey@msasafety.com>
2019-02-19 11:22:09 +0100

    ndnup: added helper function for determining size of interest/data packets

---
+++ include/helper.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2019 Safety IO 
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @file        helper.h
+ * @brief       Helper functions for NDNup 
+ * @author      Michael Frey <michael.frey@safetyio.com>
+ * @copyright   MIT License
+ * @addtogroup  NDNup Minimal Standalone NDN Packet Parser
+ * @{
+ */
+
+#ifndef HELPER_H
+#define HELPER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static inline size_t get_var_size(uint32_t var);
+
+static inline size_t get_block_size(const uint32_t type, const uint32_t payload_size)
+{
+    return payload_size + get_var_size(type) + get_var_size(payload_size);
+};
+
+static inline size_t get_var_size(uint32_t var)
+{
+    if (var < 253) {
+        return 1;
+    }
+
+    if (var <= 0xFFFF) {
+        return 3;
+    }
+
+    return 5;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HELPER_H */
+
+/**
+ * @}
+ */
