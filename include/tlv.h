c681a78c7d6882d3399b4b766d1a53f0ecd118e4
Michael Frey <michael.frey@msasafety.com>
2019-02-21 18:43:29 +0100

    ndnup: added function 'uint16_decode'

--- include/tlv.h
+++ include/tlv.h
@@ -129,6 +129,18 @@ size_t nonnegative_int_length(uint32_t value);
 int8_t tlv_boolean_encode(buffer_write_t *out, tlfield_t type);
 
 
+/**
+ * @brief       Encodes a boolean value
+ *
+ * @param[in]   in    input buffer to read
+ * @param[out]  value value to write to 
+ * @retval      0 on succes
+ * @retval      -1 @p in was NULL 
+ * @retval      -2 @p value was NULL 
+ * @retval      -3 Can't decode uint16_t from buffer @p in
+ */
+int8_t uint16_decode(buffer_read_t *in, uint16_t *value);
+
 #ifdef __cplusplus
 }
 #endif
--- src/tlv_decode.c
+++ src/tlv_decode.c
@@ -96,4 +96,27 @@ int8_t tlfield_decode(buffer_read_t *in, tlfield_t *field)
     return result;
 }
 
+int8_t uint16_decode(buffer_read_t *in, uint16_t *value)
+{
+    int8_t result = -1;
+
+    if (in) {
+        result = -2;
+
+        if (value) {
+            result = -3;
+
+            if ((in->offset + 2) <= in->length) {
+                result = 0;
+
+                *value = ((uint16_t)in->buffer[in->offset] << 8) +
+                    in->buffer[in->offset+1]; 
+
+                in->offset += 2;
+            }
+        }
+    }
+
+    return result;
+}
 
