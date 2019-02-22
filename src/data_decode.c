5beb3342915f656cc75341959455072dff89a210
Cenk Gündoğan <mail+dev@gundogan.net>
2019-02-22 13:27:45 +0100

    data_ test: add decode func for content

--- src/data_decode.c
+++ src/data_decode.c
@@ -4,10 +4,14 @@
 int8_t metainfo_decode(ndn_data_t *data, buffer_read_t *in)
 {
     int8_t result = 0;
-    tlfield_t length = tlfield_decode(in, &length);
-    tlfield_t field;
+    tlfield_t length = 0;
 
-    while(in->offset < in->length) {
+    tlfield_decode(in, &length);
+    size_t max_offset = in->offset + length;
+
+    tlfield_t field = 0;
+
+    while(in->offset < max_offset) {
         if ((result = tlfield_decode(in, &field)) != 0) {
             /* TODO */
             return result;
@@ -33,6 +37,16 @@ int8_t metainfo_decode(ndn_data_t *data, buffer_read_t *in)
     return 0;
 }
 
+int8_t content_decode(ndn_data_content_t *content, buffer_read_t *in)
+{
+    tlfield_t length = 0;
+    tlfield_decode(in, &length);
+    content->length = length;
+    buffer_read_block(in, (uint8_t *)content->value, length);
+
+   return 0;
+}
+
 int8_t data_decode(ndn_data_t *data, buffer_read_t *in)
 {
     int8_t result = -1;
@@ -49,7 +63,7 @@ int8_t data_decode(ndn_data_t *data, buffer_read_t *in)
             if ((result = tlfield_decode(in, &field)) == 0) {
                 result = -3;
 
-                /** check if the buffer actually contains an interest */
+                /** check if the buffer actually contains an data */
                 if (field == tlv_data) {
                     /** read the length of the data */
                     tlfield_decode(in, &field);
@@ -67,9 +81,12 @@ int8_t data_decode(ndn_data_t *data, buffer_read_t *in)
 
                         if (field == tlv_meta_info) {
                             metainfo_decode(data, in);
+                            data->metainfo_enabled = 1;
                         }
 
                         if (field == tlv_content) {
+                            content_decode(&data->content, in);
+                            data->content_enabled = 1;
                         }
                     }
 
